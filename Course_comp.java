51f26b000a1c080c85cd799bbadf5ba4590d4a38
Adiba Tahsin <adiba0tahsin@gmail.com>
2025-09-21 13:40:23 +0600

    new merge

--- Course_comp.java
+++ Course_comp.java
@@ -8,73 +8,77 @@ public interface Course_comp
 class lesson implements Course_comp
 {
     String lesson_name;
-    String instructorName;
+    int lesson_duration;
+    String instructor;  // CONFLICT: Added field in same location
 
-    lesson(String lesson_name, String lesson_duration)
+    lesson(String lesson_name, int lesson_duration, String instructor)  // CONFLICT: Changed constructor
     {
         this.lesson_name = lesson_name;
-        this.instructorName = instructorName;
+        this.lesson_duration = lesson_duration;
+        this.instructor = instructor;  // CONFLICT: Added initialization
     }
 
     public void showDetails()
     {
-        System.out.println("Lesson Name: " + lesson_name + ", Instructor: " + instructorName);
+        // CONFLICT: Changed output format in same method
+        System.out.println("Lesson: " + lesson_name + " | Time: " + lesson_duration + " minutes | By: " + instructor);
     }
-
-    
 }
 
 class CourseModule implements Course_comp
 {
     ArrayList<lesson> Lessons = new ArrayList<>();
     String module_name;
-    ArrayList<CourseModule>modules = new ArrayList<>();
-    CourseModule(String module_name,ArrayList<lesson> Lessons,ArrayList<CourseModule>modules)
+    ArrayList<CourseModule> modules = new ArrayList<>();
+    int difficultyLevel;  // CONFLICT: Added field
+    
+    CourseModule(String module_name, ArrayList<lesson> Lessons, ArrayList<CourseModule> modules, int difficultyLevel)  // CONFLICT: Changed constructor
     {
         this.module_name = module_name;
         this.Lessons = Lessons;
-        this.modules=modules;
+        this.modules = modules;
+        this.difficultyLevel = difficultyLevel;  // CONFLICT: Added initialization
     }
+    
     public void showDetails()
     {
-        System.out.println("Module Name: " + module_name);
+        // CONFLICT: Changed output format in same method
+        System.out.println("=== MODULE: " + module_name + " (Level: " + difficultyLevel + ") ===");
         for (lesson l : Lessons)
         {
             l.showDetails();
         }
-        for(CourseModule m:modules)
+        for(CourseModule m : modules)
         {
             m.showDetails();
         }
     }
 }
 
-class Sample_course_module{
+class Sample_course_module {
     public static void main(String[] args) {
-        lesson l1 = new lesson("Introduction to Java","Bhuski");
-        lesson l2 = new lesson("OOP Concepts", "chutki");
-        lesson l3 = new lesson("Data Structures", "rani indumati");
+        // CONFLICT: Changed constructor calls
+        lesson l1 = new lesson("Introduction to Java", 30, "Dr. Smith");
+        lesson l2 = new lesson("OOP Concepts", 45, "Prof. Johnson");
+        lesson l3 = new lesson("Data Structures", 40, "Dr. Williams");
+        
         ArrayList<lesson> lessons1 = new ArrayList<>();
         lessons1.add(l1);
         lessons1.add(l2);
+        
         ArrayList<lesson> lessons2 = new ArrayList<>();
         lessons2.add(l3);
-        CourseModule module1 = new CourseModule("Java Basics", lessons1,new ArrayList<>());
-        CourseModule module2 = new CourseModule("Advanced Topics", lessons2,new ArrayList<>());
+        
+        // CONFLICT: Changed constructor calls
+        CourseModule module1 = new CourseModule("Java Basics", lessons1, new ArrayList<>(), 1);
+        CourseModule module2 = new CourseModule("Advanced Topics", lessons2, new ArrayList<>(), 3);
+        
         ArrayList<CourseModule> subModules = new ArrayList<>();
         subModules.add(module1);
         subModules.add(module2);
-        CourseModule mainCourse = new CourseModule("Complete Java Course", new ArrayList<>(),subModules);
+        
+        // CONFLICT: Changed constructor call
+        CourseModule mainCourse = new CourseModule("Complete Java Course", new ArrayList<>(), subModules, 2);
         mainCourse.showDetails();
     }
 }
-
-
-// Output:
-// Module Name: Complete Java Course
-// Module Name: Java Basics
-// Lesson Name: Introduction to Java, Instructor: Bhuski
-// Lesson Name: OOP Concepts, Instructor: chutki
-// Module Name: Advanced Topics
-// Lesson Name: Data Structures, Instructor: rani indumati
-// Process finished with exit code 0
